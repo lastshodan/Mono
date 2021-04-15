cdaa394ab38733b093ddc0368fbd5f65261d54e3
curtis <chiegithub@gmail.com>
2021-04-15 23:34:38 +0300

    Update MonoRewardsDistributor.sol

--- contracts/utilities/MonoRewardsDistributor.sol
+++ contracts/utilities/MonoRewardsDistributor.sol
@@ -139,10 +139,10 @@ contract MonoRewardsDistributor is IRewardsDistributor, CoreRef, Timed {
     }
 
     
-    // Represents the integral of 0.9R/d - 0.9R/d^0.9 x dx from t to d
-    // Integral equals 0.9Rx/d - Rx^0.9/d^0.9
-    // Evaluated at t = 0.9R*t/d (start) - R*t^0.9/d^0.9 (end)
-    // Evaluated at d = 0.9R - R = R
+    // Represents the integral of 2R/d - 2R/d^2 x dx from t to d
+    // Integral equals 2Rx/d - Rx^2/d^2
+    // Evaluated at t = 2R*t/d (start) - R*t^2/d^2 (end)
+    // Evaluated at d = 2R - R = R
     // Solution = R - (start - end) or equivalently end + R - start (latter more convenient to code)
 
     function _unreleasedReward(
@@ -151,14 +151,13 @@ contract MonoRewardsDistributor is IRewardsDistributor, CoreRef, Timed {
         uint256 _time
     ) internal pure returns (uint256) {
     
-        //Decay rate in days
-        Decimal.D256 memory decayRate= Decimal.ratio(9,10);
+       
         
-        // 0.9R*t/d
+        // 2R*t/d
         Decimal.D256 memory start =
-            Decimal.ratio(_totalReward, _duration).mul(decayRate).mul(_time);
+            Decimal.ratio(_totalReward, _duration).mul(2).mul(_time);
 
-        // R*t^0.9/d^0.9
+        // R*t^2/d^2
         Decimal.D256 memory end =
             Decimal.ratio(_totalReward, _duration).div(_duration).mul(
                 _time * _time
